10afbc5222fb563ec5dd20e5c2c33ac769de90e7
francesanne <francesanne.riconalla@cit.edu>
2026-03-25 16:47:40 +0800

    chore: add Google API client and JWT dependencies for OAuth support

--- backend/src/main/java/edu/cit/riconalla/dentalink/controller/AuthController.java
+++ backend/src/main/java/edu/cit/riconalla/dentalink/controller/AuthController.java
@@ -5,15 +5,19 @@ import edu.cit.riconalla.dentalink.dto.LoginResponse;
 import edu.cit.riconalla.dentalink.dto.RegisterRequest;
 import edu.cit.riconalla.dentalink.service.UserService;
 import org.springframework.web.bind.annotation.*;
+import edu.cit.riconalla.dentalink.dto.GoogleLoginRequest;
+import edu.cit.riconalla.dentalink.service.GoogleService;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
     private final UserService userService;
+    private final GoogleService googleService;
 
-    public AuthController(UserService userService) {
+    public AuthController(UserService userService, GoogleService googleService) {
         this.userService = userService;
+        this.googleService = googleService;
     }
 
     @PostMapping("/register")
@@ -28,6 +32,14 @@ public class AuthController {
     public LoginResponse login(@RequestBody LoginRequest request) {
 
         String token = userService.login(request);
+
+        return new LoginResponse(token);
+    }
+
+    @PostMapping("/google")
+    public LoginResponse googleLogin(@RequestBody GoogleLoginRequest request) {
+
+        String token = googleService.loginWithGoogle(request.getIdToken());
 
         return new LoginResponse(token);
     }
