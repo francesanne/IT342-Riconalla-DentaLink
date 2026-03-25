3d5b6aee03a8fbc9d53ba3b21e392e3b68ddc68e
francesanne <francesanne.riconalla@cit.edu>
2026-03-25 21:32:45 +0800

    feat(web): configure GoogleOAuthProvider in main entry point

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import './styles/global.css'  // Changed to our new global styles
+import { GoogleOAuthProvider } from '@react-oauth/google'
+import './styles/global.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <GoogleOAuthProvider clientId="573816851730-23qfd86kjha2ahkv57cdfaoakk2bm4t8.apps.googleusercontent.com">
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
