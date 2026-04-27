258d145e2a8c7a26b604eb1ca058151878a7577d
francesanne <francesanne.riconalla@cit.edu>
2026-04-27 20:14:26 +0800

    fix(dentists): fix @Table name to plural and add explicit @Column mappings per C-1

--- backend/src/main/java/edu/cit/riconalla/dentalink/entity/Dentist.java
+++ backend/src/main/java/edu/cit/riconalla/dentalink/entity/Dentist.java
@@ -5,18 +5,22 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Table(name = "dentist")
+@Table(name = "dentists")
 @Getter
 @Setter
 public class Dentist {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "dentist_id")
     private Long dentistId;
 
+    @Column(name = "dentist_name")
     private String dentistName;
 
+    @Column(name = "dentist_specialization")
     private String dentistSpecialization;
 
+    @Column(name = "dentist_status")
     private String dentistStatus;
 }
